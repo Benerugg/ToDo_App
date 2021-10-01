6aa761657b176e701f52797fe822fcbb09707fc8
MStillahn <mstillahn1@my.hpu.edu>
2021-10-01 00:35:16 +0000

    small design changes

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,5 @@
 class TodoList < ApplicationRecord
+  validates :title, presence: true
   CATEGORY =['Private','School','Family','Work']
   PRIORITY = [['High',{:style => "background-color: Red"}],'Medium','Low']
 end
